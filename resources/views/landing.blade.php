64142deb26ec365f0e645ab2a325bb0e4d48137a
44x1carbon <b4079@oic.jp>
2016-09-05 01:31:11 +0900

    [uodate] デザインと内容を修正
    ヘッダーをfixedに
    アバウトを追加

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,15 +8,50 @@
 
 @section('content')
 <div class="top-image">
-  <img src="{{{asset('/img/top-image.jpg')}}}" alt="" />
+  <img src="{{{asset('/assets/img/top-img.jpg')}}}" alt="" />
   <div class="top-logo">
     <div>
-      <img src="{{{asset('/img/logo.png')}}}" alt="" />
+      <img src="{{{asset('/assets/img/logo.png')}}}" alt="" />
     </div>
     <p>
       人と人を繋ぎ、輪を広げていく
     </p>
   </div>
+</div>
+<div class="about-section">
+    <h1 class="heading">.Linkerとは？</h1>
+    <div class="about-content">
+      <section>
+        <h2><i class="fa fa-shield" aria-hidden="true"></i>初めての方へ</h2>
+        <p>
+          このサービスは学内のイベントを管理するためのサービスです。
+          イベントを開催、参加することで他コースの学生や先輩、後輩との繋がれる環境を提供します。
+        </p>
+      </section>
+      <section>
+        <h2><i class="fa fa-users" aria-hidden="true"></i>三人寄れば文殊の知恵</h2>
+        <p>
+          ことわざにもあるように、1人では難しいことも、みんなでやれば出来ます。
+          一緒に勉強したり、切磋琢磨し技術を磨く"仲間""を見つけましょう。
+        </p>
+      </section>
+      <section>
+        <h2><i class="fa fa-question-circle" aria-hidden="true"></i>どういった時に使うのか</h2>
+        <ul>
+          <li>勉強したことをアウトプットしたい時</li>
+          <li>一緒に自習する仲間を探したい時</li>
+          <li>遊戯王の大会を開催したい時</li>
+          <li>モンストのチームを募集したい時</li>
+        </ul>
+      </section>
+      <section>
+        <h2><i class="fa fa-tags" aria-hidden="true"></i>自分に合ったイベント</h2>
+        <p>
+          .Linkerでは自分に合ったイベントをすぐに見つけるための機能があります。
+          イベントやプロフィールに好きなタグを設定することで、自分に合ったイベントを探すことが出来ます。
+        </p>
+      </section>
+    </div>
 </div>
 <div class="event-section">
     <section>
@@ -35,28 +70,5 @@
             @endforeach
         </div>
     </section>
-</div>
-<div class="about-section">
-    <h1 class="heading">.Linkerとは？</h1>
-    <div class="about-content">
-      <section>
-        <h2><i class="fa fa-shield" aria-hidden="true"></i>初めての方へ</h2>
-        <p>
-
-        </p>
-      </section>
-      <section>
-        <h2>初めての方へ</h2>
-        <p>
-
-        </p>
-      </section>
-      <section>
-        <h2>初めての方へ</h2>
-        <p>
-
-        </p>
-      </section>
-    </div>
 </div>
 @endsection
