cd6504f5da55070f810907b1df60d876beda956c
44x1carbon <b4079@oic.jp>
2016-11-17 01:40:06 +0900

    posttest作成

--- app/Http/Routes/mock.php
+++ app/Http/Routes/mock.php
@@ -1,11 +1,18 @@
 <?php //Created at: 16-09-05 13:45:33
 
+use Illuminate\Http\Request;
+
 Route::get('/mock/landing','MockController@landing');
 
 Route::get('/mock/mypage','MockController@mypage');
 
 Route::get('/mock/vuetest',function(){
-  return view('vuetest');
+  return view('mock.vuetest');
 });
+
+Route::post('/mock/posttest',[ 'as' => 'posttest', function(Request $request) {
+  $data['requests'] = $request->all();
+  return view('mock.posttest', $data);
+}]);
 
 Route::get('/mock/organize','MockController@organize');
