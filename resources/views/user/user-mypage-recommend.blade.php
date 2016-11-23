547589fc0770884586346347b0a937237c164d90
44x1carbon <b4079@oic.jp>
2016-11-23 23:18:54 +0900

    my-page.css追加

--- resources/views/user/user-mypage-recommend.blade.php
+++ resources/views/user/user-mypage-recommend.blade.php
@@ -1,5 +1,11 @@
 @extends('layout.app')
 
+@section('head')
+	<link rel="stylesheet" href="{{{asset('/css/my-page.css')}}}" media="screen" title="no title" charset="utf-8">
+@endsection
+
 @section('content')
 <h1>マイページ　おすすめイベント</h1>
+@include('components/my-profile')
+@inclide('components/my-tag-list', [ 'user' => Auth::user()])
 @endsection
