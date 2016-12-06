e3663c5b3e356377d9aa6f2fa2bf40cfc16289a9
44x1carbon <b4079@oic.jp>
2016-12-06 15:00:27 +0900

    user-entry-stepコンポーネント作成

---
+++ resources/views/components/user-entry-step.blade.php
@@ -0,0 +1,13 @@
+<div class="register-step">
+	<div class="item {{ $current === 'profile'? 'active': '' }}">
+	プロフィール登録
+	</div>
+	<i class="fa fa-arrow-right" aria-hidden="true"></i> 
+	<div class="item {{ $current === 'favtag'? 'active': '' }}">
+	お気に入りタグ登録
+	</div>
+	<i class="fa fa-arrow-right" aria-hidden="true"></i>
+	<div class="item {{ $current === 'confirm'? 'active': '' }}">
+	確認
+	</div>      
+</div>
