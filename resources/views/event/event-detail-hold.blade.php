eaf27353daa69189f5579881a0e52ef74f768690
ymgn <gonyamagon@gmail.com>
2016-12-16 14:28:42 +0900

    update イベント詳細の開催者bladeをstatusごとに分岐

--- resources/views/event/event-detail-hold.blade.php
+++ resources/views/event/event-detail-hold.blade.php
@@ -1,9 +1,20 @@
 @extends('event.event-detail-layout')
 
 @section('hold')
-
+@if ($event[0]->status == "open")
+<a>イベント編集</a>
+<button>イベント中止</button>
+@elseif ($event[0]->field == "close")
+<a>イベント編集</a>
+<button>イベント公開</button>
+@endif
 @endsection
 
 @section('subscription')
-  <button type="button">申し込み</button>
+@if ($event[0]->status == "open")
+<button type="button" disabled="disabled">申し込み</button>
+@elseif ($event[0]->field == "close")
+<button type="button" >申し込み</button>
+@endif
+
 @endsection
