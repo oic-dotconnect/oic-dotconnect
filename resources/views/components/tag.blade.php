ed05c70d8c20868e531d51472d8d5a18fc3d67b9
44x1carbon <b4079@oic.jp>
2017-01-03 15:03:00 +0900

    divでラップ

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ route('event-search',[ 'tags' => $tag->name ]) }}">{{ $tag->name }}</a>
+<div class="tag"><a href="{{ route('event-search',[ 'tags' => $tag->name ]) }}">{{ $tag->name }}</a></div>
