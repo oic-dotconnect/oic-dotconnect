09f7f39588770cbf4d7d529348990805281e1f61
44x1carbon <b4079@oic.jp>
2017-01-04 21:59:37 +0900

    通知設定

--- resources/views/user/user-setting-notice.blade.php
+++ resources/views/user/user-setting-notice.blade.php
@@ -2,50 +2,71 @@
 
 @section('head')
   <link rel="stylesheet" href="{{{asset('css/user-setting/user-setting-common.css')}}}" media="screen" title="no title" charset="utf-8">  
+  <link rel="stylesheet" href="{{{asset('css/user-setting/user-setting-notice.css')}}}" media="screen" title="no title" charset="utf-8">  
 @endsection
 
 @section('content')
     <div class="wrapper">
-        <h1 class="user-setting-title">ユーザー設定</h1>
-        <div class="top">
-            <h1>アカウント設定</h1>
-        </div>
-        <div class="primary">
-            <div class="inner">
-                @include('components/user-setting-menu', [ 'current' => 'notice' ])
-                <div class="acount-config">
-                    <div class="withdrawal">
-                        <h2>メール通知</h2>
-                    </div>
-                    <div class="wording">
-                        <h2>・開催イベント</h2>
-                        <p><input type="checkbox" name="join-nottification" value="join-nottification"> 参加通知</p>
-                        <p>*</p>
-                        <p><input type="checkbox" name="not-join-nottification" value="not-join-nottification"> 参加キャンセル通知</p>
-                        <p>*</p>
-                        <h2>・お気に入りタグ</h2>
-                        <p><input type="checkbox" name="tag-regular-nottification" value="tag-regular-nottification"> 参加通知</p>
-                        <p>*</p>
-                        <p>個別タグ通知</p>
-                        <p>*</p>
-                        <div class="button-group">
-                            <button>すべて解除</button>
-                            <button>すべて選択</button>
+        <h1 class="user-setting-title">ユーザー設定</h1>        
+        @include('components/user-setting-menu', [ 'current' => 'notice' ])        
+        <h2 class="user-setting-sub-title">メール通知設定</h2>
+        {{ Form::open([
+            'route' => 'post-user-setting-notice',            
+        ])}}
+            <div class="">
+                    <div class="hold-event-notice box notice-box">
+                        <h2 class="box-title red">開催イベント</h2>
+                        <div class="notice-caption">
+                            自分が開催しているイベントの参加者が増えた場合（参加）、減った場合（キャンセル）に通知します。
                         </div>
-                        @foreach(Auth::user()->tags as $tag)
-                            <input type="checkbox" name="タグ名" class="candidate-item" value={{ $tag->id }}>{{ $tag->name }}
-                        @endforeach
+                        <div class="notice-form">
+                            <div class="notice-check">
+                                <input type="checkbox" id="notice-join" name="notice-join" {{ $user->event_join_notice? 'checked': '' }}><label for="notice-join">参加通知</label>
+                            </div>
+                            <div class="notice-check">
+                                <input type="checkbox" id="notice-cancel" name="notice-cancel" {{ $user->event_cancel_notice? 'checked': '' }}><label for="notice-cancel">キャンセル通知</label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="paging">
-                        <a href="#">マイページへ戻る</a>
-                        <button type="button">変更</button>
+                    <div class="regular-notice box notice-box">
+                        <h2 class="box-title yellow">定期通知</h2>
+                        <div class="notice-caption">
+                            お気に入りのタグなどから、あなたにオススメのイベントを1週間に一度メールでお知らせします。
+                        </div>
+                        <div class="notice-form">
+                            <div class="notice-check">
+                                <input type="checkbox" id="notice-regular" name="notice-regular" {{ $user->regular_notice? 'checked': '' }}><label for="notice-reglar">定期通知</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tag-notice box notice-box">
+                        <h2 class="box-title green">タグ</h2>
+                        <div class="notice-caption">
+                            お気に入りにしているタグの付いたイベントが登録された場合に通知します。個別に設定することができます。
+                        </div>
+                        <div class="notice-form">                    
+                            <div class="notice-tags">
+                                @foreach($user->tags as $tag)                            
+                                    <div class="tag"><input type="checkbox" id="{{ 'notice-tag-' . $tag->id }}" name="tag-notice[{{ $tag->id }}]" {{ $tag->pivot->noticed? 'checked': '' }}><label for="{{ 'notice-tag-' . $tag->id }}">{{ $tag->name }}</label><wbr></div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+            </div>                
+            <div class="row info">
+                <div class="col">          
+                    <div class="row">
+                        <div class="info-left">
+                            <a href="{{ route('user-mypage-recommend') }}" class="button cancell left">キャンセル</a>
+                        </div>
+                        <div class="info-right">                
+                            <button type="submit" class="change button right">変更</button>
+                        </div>
                     </div>
                 </div>
-                <!-- acoutn-config -->
-            </div>
-            <!-- inner -->
-        </div>
-        <!-- parimary -->
+                </div>
+            </div> 
+        {{ Form::close() }}  
     </div>
 @endsection
 
