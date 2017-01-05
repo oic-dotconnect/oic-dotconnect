27f49bc0c2fe92a6cb96071d0d350ecdb2acc90b
44x1carbon <b4079@oic.jp>
2017-01-05 15:57:00 +0900

    デザイン適応

--- resources/views/event/event-entry.blade.php
+++ resources/views/event/event-entry.blade.php
@@ -1,92 +1,85 @@
 @extends('layout.app')
 
 @section('head')
-
+    <link rel="stylesheet" href="{{{asset('css/event-entry.css')}}}" media="screen" title="no title" charset="utf-8">
 @endsection
 
 @section('content')
-<div class="wrapper">
-    <div class="top">
-        <h1>イベント登録</h1>
-    </div>
-    <!-- div.top -->
-    <div class="primary">
-        <div class="inner">
-            <div class="entry-form">
-            {{Form::open(['route' => 'post-event-entry'])}}
-                <div class="entry-event-name">
-                    <h2 class="form-header">イベント名</h2>
+<div class="wrapper">    
+    <h1 class="title">イベント登録</h1>
+    <div class="entry-form">
+    {{Form::open(['route' => 'post-event-entry'])}}
+        <div class="row">
+            <div class="col top-left">
+                <div class="entry-event-name box">
+                    <h2 class="form-header box-title red">イベント名</h2>
                     <input class="entry-input" type="text" name="name">
                 </div>
                 <!-- entry-form -->
-                <div class="entry-field">
-                    <h2 class="form-header">分野</h2>
-                    <select class="entry-item">
-                        <option value="">選択してください</option>
-                        <option value="IT">IT</option>
-                        <option value="デザイン">デザイン</option>
-                        <option value="ゲーム">ゲーム</option>
-                        <option value="その他">その他</option>
-                    </select>
-                    <!-- input-entry-field entry-item -->
+                <div class="tag-field box">
+                    <h2 class="box-title green">タグ</h2>
+                    <tag-choice></tag-choice>
+                </div>
+                <div class="entry-field box">
+                    <h2 class="form-header box-title yellow">分野</h2>
+                    <div>
+                        <input type="radio" name="field" id="field-it" value="it"><label for="field-it">IT</label>
+                        <input type="radio" name="field" id="field-game" value="game"><label for="field-game">ゲーム</label>
+                        <input type="radio" name="field" id="field-design" value="design"><label for="field-design">デザイン</label>
+                        <input type="radio" name="field" id="field-movie" value="movie"><label for="field-movie">映像</label>
+                        <input type="radio" name="field" id="field-other" value="other" checked><label for="field-other">その他</label>
+                    </div>           
                 </div>
                 <!-- entry-field -->
-                <tag-choice></tag-choice>
-                <!-- entry-tag -->
-                <div class="entry-date">
-                    <div class="entry-item-date">
+            </div>
+            <div class="col top-right">
+                <div class="entry-date box">
+                    <h2 class="box-title blue">開催日時</h2>
+                    <div class="entry-opendate-date">
                         <div class="entry-date-group">
-                            <h2 class="form-header">開催日</h2>
+                            <h3 class="form-header">開催日</h3>
                             <input class="entry-input-date" type="date" name="opening_date">
                         </div>
                     </div>
                     <!-- entry-item-date -->
-                    <div class="entry-item-date">
-                        <span></span>
-                        <div class="entry-date-group">
-                            <h2 class="form-header">開始時間</h2>
+                    <div class="entry-opendate-time">                        
+                        <div class="entry-opendate-starttime">
+                            <h3 class="form-header">開始時間</h3>
                             <input class="entry-input-date" type="time" name="start_at">
                         </div>
-                        <!-- entry-date-group -->
-                    </div>
-                    <!-- entry-item-date -->
-                    <div class="entry-item-date">
-                        <span></span>
-                        <div class="entry-date-group">
-                            <h2 class="form-header">終了時間</h2>
+                        <div class="entry-opendate-endtime">
+                            <h3 class="form-header">終了時間</h3>
                             <input class="entry-input-date" type="time" name="end_at">
                         </div>
                         <!-- entry-date-group -->
                     </div>
-                    <!-- entry-item-date -->
+                    <!-- entry-item-date -->                    
                 </div>
-                <!-- entry-date -->
-                <room-choice></room-choice>
-                <!-- entry-room -->
-                <div class="entry-recruitment">
-                    <h2 class="form-header">募集期間</h2>
-                    <div class="entry-recruitment-group">
-                        <div class="entry-start-group">
-                            <div class="entry-start-date">
-                                <h2 class="form-header">開始日</h2>
+                <!-- entry-date -->        
+                <div class="entry-recruitment box">
+                    <h2 class="form-header box-title purple">募集期間</h2>
+                    <div class="">
+                        <div class="entry-recruitmen-start-group entry-recruitmen-group">
+                            <div class="entry-recruitmen-start-date">
+                                <h3 class="form-header">開始日</h3>
                                 <input class="start-date" type="date" name="recruit_start_date">
                             </div>
                             <!-- entry-start-date -->
-                            <div class="entry-start-time">
-                                <h2 class="form-header">開始時間</h2>
+                            <div class="entry-recruitmen-start-time">
+                                <h3 class="form-header">開始時間</h3>
                                 <input class="start-time" type="time" name="recruit_start_time">
                             </div>
                             <!-- entry-start-time -->
                         </div>
                         <!-- entry-start-group -->
-                        <div class="entry-end-group">
-                            <div class="entry-end-date">
-                                <h2 class="form-header">終了日</h2>
+                        <div class="entry-recruitmen-end-group entry-recruitmen-group">
+                            <div class="entry-recruitmen-end-date">
+                                <h3 class="form-header">終了日</h3>
                                 <input class="end-date" type="date" name="recruit_end_date">
                             </div>
                             <!-- entry-start-date -->
-                            <div class="entry-end-time">
-                                <h2 class="form-header">終了時間</h2>
+                            <div class="entry-recruitmen-end-time">
+                                <h3 class="form-header">終了時間</h3>
                                 <input class="end-time" type="time" name="recruit_end_time">
                             </div>
                             <!-- entry-start-time -->
@@ -96,40 +89,43 @@
                     <!-- entry-recruitment-group -->
                 </div>
                 <!-- entry-recruitment -->
-                <div class="entry-capcity">
-                    <h2 class="form-header">定員</h2>
-                    <input class="entry-input entry-capcity-input" type="text" name="capacity">
-                    <span>人</span>
-                </div>
-                <!-- entry-capcity -->
-                <div class="entry-description">
-                    <h2 class="form-header">説明</h2>
-                    <div class="entry-description-group">
-                        <!--<div class="entry-description-radio">
-                            <input type="radio" name="editer" value="">
-                            <p>リッチテキストエディタ</p>
-                        </div>-->
-                        <!-- entry-description-radio -->
-                        <!--<div class="entry-description-radio">
-                            <input type="radio" name="editer" value="">
-                            <p>Markdown</p>
-                        </div>-->
-                        <!-- entry-description-radio -->
-                    </div>
-                    <!-- entry-description-group -->
-                    <texteditor></texteditor>
-                </div>
-                <!-- entry-description -->
-            <button type="submit" name="status" value="close">下書きを保存する</button>
-            <a href="#mypage">キャンセル</a>
-            <button type="submit" name="status" value="open">公開</button>
-            {{Form::close()}}
             </div>
-            <!-- entry-from -->
         </div>
-        <!-- inner -->
+        <!-- entry-tag -->
+        <div class="box">
+            <h2 class="box-title orange">開催教室</h2>
+            <room-choice></room-choice>
+        </div>
+        <!-- entry-room -->
+        <div class="entry-capcity box">
+            <h2 class="form-header box-title cyan">定員</h2>
+            <input class="entry-input entry-capcity-input" type="number" name="capacity">
+            <span>人</span>
+        </div>
+        <!-- entry-capcity -->
+        <div class="entry-description box">
+            <h2 class="form-header box-title pink">説明</h2>
+            <div class="entry-description-group">               
+            </div>
+            <!-- entry-description-group -->
+            <texteditor></texteditor>
+        </div>
+        <!-- entry-description -->
+    <div class="info">
+        <div class="info-top">
+            <button type="submit" name="status" value="close" class="button">下書きを保存する</button>
+        </div>
+        <div class="info-bottom">
+            <div class="info-left">
+                <a href="#mypage" class="button cancell">キャンセル</a>
+            </div>
+            <div class="info-right">
+                <button type="submit" name="status" value="open" class="button open">公開</button>
+            </div>
+        </div>
     </div>
-    <!-- primary -->
+    {{Form::close()}}
+    </div>
+    <!-- entry-from -->  
 </div>
-<!-- primary -->
 @endsection
