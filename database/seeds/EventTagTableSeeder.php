78d7504f7d672ef69f4f060e13c136eace8180aa
44x1carbon <b4079@oic.jp>
2017-01-10 18:14:14 +0900

    修正

--- database/seeds/EventTagTableSeeder.php
+++ database/seeds/EventTagTableSeeder.php
@@ -12,14 +12,12 @@ class EventTagTableSeeder extends Seeder
      */
     public function run()
     {
-      $z=1;
       for($i=1;$i<11;$i++){
         for($j=1;$j<5;$j++){
         DB::table('EVENT_TAG')->insert([
           'event_id'=> $i,
-          'tag_id' => $z
-          ]);
-        $z++;
+          'tag_id' => $j
+          ]);        
         }
       }
 
