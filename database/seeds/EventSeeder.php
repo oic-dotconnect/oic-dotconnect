116f678f89b8690c061c4ef9170c27e702239352
44x1carbon <b4079@oic.jp>
2017-01-14 02:42:12 +0900

    モックデータ

--- database/seeds/EventSeeder.php
+++ database/seeds/EventSeeder.php
@@ -19,135 +19,42 @@ class EventSeeder extends Seeder
     */
     public function run()
     {
-      $faker = Faker::create('ja_JP');
-      $fields = ['it', 'design', 'movie', 'game', 'other'];
-      $room = ['5A', '9D-1','3A'];
-      $bool = [true, false];
-      $organizer_id = 1;
-      for($i = 0; $i < 2; $i++)
-      {
-        $datetime = $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+1 years');
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        App\Models\Event::create([
-          'code' => array_reduce(range(1, 8), function($p){ return $p.str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz')[0]; }),
-          'name' => 'Laravel勉強会' . $i,
-          'field' => 'it',
-          'description' => '<h1>説明</h1>',
-          'opening_date'=>$date,
-          'start_at' => $time,
-          'end_at' => date('H:i:s', strtotime($time.'+2 hour')),
-          'place' => $faker->randomElement($room),
-          'capacity' => $faker->numberBetween(10,100),
-          'status' => 'open',
-          'organizer_id' => $organizer_id,
-          'open_date' => date('Y-m-d',strtotime($date.'-2 week')),
-          'recruit_start_date' => date('Y-m-d',strtotime($date.'-4 week')),
-          'recruit_end_date' => date('Y-m-d',strtotime($date.'-1 week')),
-          'recruit_start_time' => $time,
-          'recruit_end_time' => date('H:i:s',strtotime($time.'+2 hour')),
-        ]);
-        $organizer_id++;
+      $json = file_get_contents(__DIR__ . '/../mock-data/mock-event.json');
+      if ($json === false) {
+        throw new \RuntimeException('file not found.');
       }
-      for($i = 0; $i < 2; $i++)
-      {
-        $datetime = $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+1 years');
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        App\Models\Event::create([
-          'code' => array_reduce(range(1, 8), function($p){ return $p.str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz')[0]; }),
-          'name' => 'Vue.Js勉強会' . $i,
-          'field' => 'it',
-          'description' => '<h1>説明</h1>',
-          'opening_date'=>$date,
-          'start_at' => $time,
-          'end_at' => date('H:i:s', strtotime($time.'+2 hour')),
-          'place' => $faker->randomElement($room),
-          'capacity' => $faker->numberBetween(10,100),
-          'status' => 'close',
-          'organizer_id' => $organizer_id,
-          'open_date' => date('Y-m-d',strtotime($date.'-2 week')),
-          'recruit_start_date' => date('Y-m-d',strtotime($date.'-4 week')),
-          'recruit_end_date' => date('Y-m-d',strtotime($date.'-1 week')),
-          'recruit_start_time' => $time,
-          'recruit_end_time' => date('H:i:s',strtotime($time.'+2 hour')),
-        ]);
-        $organizer_id++;
-      }
-      for($i = 0; $i < 2; $i++)
-      {
-        $datetime = $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+1 years');
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        App\Models\Event::create([
-          'code' => array_reduce(range(1, 7), function($p){ return $p.str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz')[0]; }),
-          'name' => 'PhotoShop勉強会' . $i,
-          'field' => 'design',
-          'description' => '<h1>説明</h1>',
-          'opening_date'=>$date,
-          'start_at' => $time,
-          'end_at' => date('H:i:s', strtotime($time.'+2 hour')),
-          'place' => $faker->randomElement($room),
-          'capacity' => $faker->numberBetween(10,100),
-          'status' => 'stop',
-          'organizer_id' => $organizer_id,
-          'open_date' => date('Y-m-d',strtotime($date.'-2 week')),
-          'recruit_start_date' => date('Y-m-d',strtotime($date.'-4 week')),
-          'recruit_end_date' => date('Y-m-d',strtotime($date.'-1 week')),
-          'recruit_start_time' => $time,
-          'recruit_end_time' => date('H:i:s',strtotime($time.'+2 hour')),
-        ]);
-        $organizer_id++;
-      }
-      for($i = 0; $i < 2; $i++)
-      {
-        $datetime = $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+1 years');
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        App\Models\Event::create([
-          'code' => array_reduce(range(1, 8), function($p){ return $p.str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz')[0]; }),
-          'name' => 'Unity勉強会' . $i,
-          'field' => 'game',
-          'description' => '<h1>説明</h1>',
-          'opening_date'=>$date,
-          'start_at' => $time,
-          'end_at' => date('H:i:s', strtotime($time.'+2 hour')),
-          'place' => $faker->randomElement($room),
-          'capacity' => $faker->numberBetween(10,100),
-          'status' => 'open',
-          'organizer_id' => $organizer_id,
-          'open_date' => date('Y-m-d',strtotime($date.'-2 week')),
-          'recruit_start_date' => date('Y-m-d',strtotime($date.'-4 week')),
-          'recruit_end_date' => date('Y-m-d',strtotime($date.'-1 week')),
-          'recruit_start_time' => $time,
-          'recruit_end_time' => date('H:i:s',strtotime($time.'+2 hour')),
-        ]);
-        $organizer_id++;
-      }
-      for($i = 0; $i < 2; $i++)
-      {
-        $datetime = $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+1 years');
-        $date = $datetime->format('Y-m-d');
-        $time = $datetime->format('H:i:s');
-        App\Models\Event::create([
-          'code' => array_reduce(range(1, 8), function($p){ return $p.str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz')[0]; }),
-          'name' => 'モンスト超獣神祭爆死大会' . $i,
-          'field' => 'other',
-          'description' => '<h1>説明</h1>',
-          'opening_date'=>$date,
-          'start_at' => $time,
-          'end_at' => date('H:i:s', strtotime($time.'+2 hour')),
-          'place' => $faker->randomElement($room),
-          'capacity' => $faker->numberBetween(10,100),
-          'status' => 'close',
-          'organizer_id' => $organizer_id,
-          'open_date' => date('Y-m-d',strtotime($date.'-2 week')),
-          'recruit_start_date' => date('Y-m-d',strtotime($date.'-4 week')),
-          'recruit_end_date' => date('Y-m-d',strtotime($date.'-1 week')),
-          'recruit_start_time' => $time,
-          'recruit_end_time' => date('H:i:s',strtotime($time.'+2 hour')),
-        ]);
-        $organizer_id++;
+      $events = json_decode($json, true);
+      foreach ($events as $event) {
+        try {        
+          $tags = $event['tags'];
+          $members = $event['members'];
+          unset($event['tags']);
+          unset($event['members']);
+          $event['organizer_id'] = App\Models\User::findCode($event["organaizer_code"])->id;
+          $event['status'] = "open";
+          unset($event['organaizer_code']); 
+          $new_event = App\Models\Event::create($event);       
+
+          foreach ($tags as $tag) {
+            $t = App\Models\Tag::where('name', $tag)->first();                    
+            if(is_null($t)) {
+              $new_tag = App\Models\Tag::create([
+                "name" => $tag
+              ]);
+              $new_event->tags()->attach($new_tag->id);
+            } else {            
+              $new_event->tags()->attach($t->id);            
+            }
+          }
+          
+          foreach ($members as $member) {
+            $id = App\Models\User::findCode($member)->id;
+            $new_event->users()->attach($id);
+          }
+        } catch(Exception $e){
+          dd($event,$e);
+        }
+        
       }
     }
 }
