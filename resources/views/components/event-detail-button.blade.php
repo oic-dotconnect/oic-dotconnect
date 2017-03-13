3bad4bb93ab80d94cf285fe238eab2ca9819e0ff
44x1carbon <b4079@oic.jp>
2017-03-13 23:38:21 +0900

    close #151

--- resources/views/components/event-detail-button.blade.php
+++ resources/views/components/event-detail-button.blade.php
@@ -1,21 +1,21 @@
 @inject('eventservice','App\Services\EventService')
 
 @if($event->status === 'stop') <!-- イベント中止状態 -->
-  <button type="button" disabled="disabled" class="button join">中止イベント</button>
+  <button type="button" disabled="disabled" class="btn stop disabled">中止イベント</button>
 @elseif($eventservice->conditionClass($event) === 'finished')
-  <button type="button" disabled="disabled" class="button join">終了</button>
+  <button type="button" disabled="disabled" class="btn finish disabled">終了</button>
 @else
   @if($current === 'join') <!-- 参加状態 -->
     {{ Form::open(['route' => ['post-event-cancel', $event->code] ]) }}
-      <button type="submit" class="button cancell">キャンセル</button>    
+      <button type="submit" class="btn cancel">キャンセル</button>    
     {{ Form::close() }}
   @elseif($current === 'viewer') <!-- 参加前状態 -->
     @if( $eventservice->isBetweenRecruit($event) )
       {{ Form::open(['route' => ['post-event-join', $event->code] ]) }}
-        <button type="submit" class="button join" >参加する</button>
+        <button type="submit" class="btn join" >参加する</button>
       {{ Form::close() }}
     @else
-      <button type="button" disabled="disabled" class="button">募集期間外</button>
+      <button type="button" disabled="disabled" class="btn disabled">募集期間外</button>
     @endif
   @elseif($current === 'guest') <!-- ログインしていない状態 -->
     <a href="{{ route('sociallogin', [ 'redirect_url' => Request::url() ]) }}" class="login-btn">
@@ -25,15 +25,15 @@
   @elseif($current === 'hold') <!-- 開催者が見た状態 -->    
     @if ($event->status == "open")
       @if( $eventservice->isBetweenRecruit($event) )        
-        <button type="button" disabled="disabled" class="button join">参加する</button>
+        <button type="button" disabled="disabled" class="btn join">参加する</button>
       @else
-        <button type="button" disabled="disabled" class="button">募集期間外</button>
+        <button type="button" disabled="disabled" class="btn disabled">募集期間外</button>
       @endif
     @elseif ($event->status == "close")
       @if( $eventservice->isBetweenRecruit($event) )
-        <button type="submit" class="button join" >参加する</button>
+        <button type="submit" class="btn join" >参加する</button>
       @else
-        <button type="button" disabled="disabled" class="button">募集期間外</button>
+        <button type="button" disabled="disabled" class="btn disabled">募集期間外</button>
       @endif
     @endif
   @endif
