09e59c838e343b2c61e1b8484601b9a7116244b0
ahaha0807 <b5331@oic.jp>
2017-05-22 16:50:18 +0900

    バグ改修 #180
    - JS の処理内にあった問題を修正
    - dateZellFill メソッドを使用しないように変更

--- resources/assets/js/components/event-item.vue
+++ resources/assets/js/components/event-item.vue
@@ -76,11 +76,7 @@
                 let opendate = this.dateFormat(date)
                 let today = new Date();
 
-                var dateZellFill = function(number) {
-                    return ("0" + number).substr(-2);
-                }
-
-                today = dateZellFill(today.getFullYear()) + '' + dateZellFill((today.getMonth() + 1)) + '' + dateZellFill(today.getDate())
+                today = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
 
                 if (today < opendate) {
                     str = '開催前'
